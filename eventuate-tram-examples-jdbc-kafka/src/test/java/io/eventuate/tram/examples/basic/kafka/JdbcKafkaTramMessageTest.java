1fb0f3da90467f4a71123f81a3a16adccde2fd5c
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-07-05 18:16:35 +0300

    Fixed dependencies.

--- eventuate-tram-examples-jdbc-kafka/src/test/java/io/eventuate/tram/examples/basic/kafka/JdbcKafkaTramMessageTest.java
+++ eventuate-tram-examples-jdbc-kafka/src/test/java/io/eventuate/tram/examples/basic/kafka/JdbcKafkaTramMessageTest.java
@@ -1,14 +1,9 @@
 package io.eventuate.tram.examples.basic.kafka;
 
 import io.eventuate.tram.examples.basic.messages.AbstractTramMessageTest;
-import io.micronaut.context.annotation.Property;
 import io.micronaut.test.annotation.MicronautTest;
 
 @MicronautTest
-//@Property(name = "micronaut.eventuate.tram.message.producer.jdbc.factory", value = "true")
-//@Property(name = "micronaut.eventuate.tram.common.producer.factory", value = "true")
-//@Property(name = "micronaut.eventuate.sql.dialect.factory", value = "true")
-//@Property(name = "micronaut.eventuate.common.jdbc.operations.factory", value = "true")
 public class JdbcKafkaTramMessageTest extends AbstractTramMessageTest {
 
 }
